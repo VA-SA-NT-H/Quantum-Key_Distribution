312fecad359aff19f6db628ff6523e27d8cb9931
VA-SA-NT-H <vasanthchandru08@gmail.com>
2025-06-08 03:27:02 +0530

    Final key function added and rwa key calculation improved

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from e91_simulator import run_e91_simulation
-from metrics import calculate_chsh, calculate_qber, calculate_key_rate
+from metrics import calculate_chsh, calculate_qber, calculate_key_rate, final_key
 from visualize import plot_chsh, plot_qber, plot_key_rate
 
 if __name__ == "__main__":
-    rounds = 1000
+    rounds = int(input("Enter number of rounds (default 1000): ") or 1000)
     results = run_e91_simulation(rounds)
-
+    print(results)
+    matched_bits = 0
+    
     chsh_value = calculate_chsh(results)
-    qber = calculate_qber(results)
-    key_rate = calculate_key_rate(results, qber)
+    qber = calculate_qber(results, matched_bits)
+    key_rate = calculate_key_rate(results, qber, rounds, matched_bits)
+    secret_key = final_key(results)
 
+    print(f"Final secret key : {secret_key}")
     print(f"\n✅ CHSH Value: {chsh_value:.3f} (Expected > 2)")
     print(f"✅ QBER: {qber * 100:.2f}%")
     print(f"✅ Final Key Rate: {key_rate:.3f}\n")
--- metrics.py
+++ metrics.py
@@ -16,16 +16,30 @@ def calculate_chsh(data):
     S = abs(E[(0, 0)] - E[(0, 1)] + E[(1, 0)] + E[(1, 1)])
     return S
 
-def calculate_qber(data):
+def calculate_qber(data, matched_bits):
     mismatch = total = 0
     for d in data:
-        a, b = d["alice_angle_idx"], d["bob_angle_idx"]
-        if a == b:
-            total += 1
-            if d["outcome"] not in ['00', '11']:
-                mismatch += 1
+        # a, b = d["alice_angle_idx"], d["bob_angle_idx"]
+        if d["outcome"] in ['00','11']:
+            matched_bits += 1
+        # if a == b:
+        total += 1
+        if d["outcome"] not in ['00', '11']:
+            mismatch += 1
+            
     return mismatch / total if total > 0 else 0
 
-def calculate_key_rate(data, qber):
-    R0 = 0.5  # Raw key rate
+def calculate_key_rate(data, qber, total_rounds, matched_bits):
+    # R0 = 0.5  # Raw key rate
+    #valid_key_bits = [r for r in data if r['used_for_key'] == True]
+    # R0 = len(valid_key_bits) / total_rounds
+    R0 = matched_bits / total_rounds
+
     return R0 * (1 - 2 * qber)
+
+def final_key(data):
+    secret_key = ''
+    omitted_key = ''
+    for d in data:
+        secret_key += '0' if d["outcome"] == '00' else '1'
+    return secret_key
